80672c917ace4bfef941cdfcf2796eec505fe2d8
Peter <petersolomon704@gmail.com>
2023-05-29 15:43:59 +0100

    Create Diaries.tsx
    Modified code

---
+++ src/components/Diaries.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const Diaries = () => {
+  const [diaries, setDiaries] = useState([]);
+  return <div></div>;
+};
+
+export default Diaries;
